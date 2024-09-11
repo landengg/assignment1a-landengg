3af00cfd480e84852b8bff23a1fe8f69622ede6e
Landen Gibson <landengibson15@gmail.com>
2024-09-11 12:38:45 -0400

    first commit

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "landengg";
+    char STUDENT_ID[] = "lkg5419";
 
     return 0 ;
 }
